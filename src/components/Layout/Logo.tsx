ecff4f41b7222f6a68bb54f8cba1f62d70e478cf
Basit Raza <>
2023-04-14 05:11:20 +0200

    chore: added freelancer dex

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <h1 className='text-2xl text-white'>
       <NavLink to='/'>
-        TL<span className='text-indigo-300'>indie</span>
+        <img src='https://uploads-ssl.webflow.com/63d95bc6235f046b968ade6f/63e37c60156dab7db5672e6f_FREELANCERDEX.jpg' />
       </NavLink>
     </h1>
   );
